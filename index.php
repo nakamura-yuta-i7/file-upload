528866642b9d7d899cb67b338c1c6906bdb813c3
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2015-09-22 13:19:05 +0000

    upload progress bar finish

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 <html>
   <head>
     <title>アップロードサンプル</title>
+    <link href="/style.css" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script type="text/javascript" src="/upload_form.js"></script>
   </head>
   <body>
     <h1>アップロードサンプル</h1>
     <p>ブラウザにファイルをドロップするとファイルをアップロードできます。</p>
-    
+    <h2>進捗状況</h2>
+    <div id="progress-bar-wrapper">
+      <div id="progress-bar"></div>
+    </div>
   </body>
 </html>
--- upload_form.js
+++ upload_form.js
@@ -1,14 +1,36 @@
 $(function(){
-  $("body").bind("drop", function(e){
+  $(document).bind("drop", function(e){
     e.preventDefault();
     // ファイル情報を取得
     var files = e.originalEvent.dataTransfer.files;
     uploadFiles(files);
+
+    console.log("drop");
+    $("body").removeClass("drag");
+
     return false;
+
   }).bind("dragenter", function(e){
     e.preventDefault();
+    console.log("dragenter");
+    $("body").addClass("drag");
+
   }).bind("dragover", function(e){
+    return false;
+
+  }).bind("dragleave", function(e){
     e.preventDefault();
+    console.log("dragleave");
+
+  }).bind("dragend", function(e){
+    console.log("dragend");
+
+  }).bind("dragstart", function(e){
+    console.log("dragstart");
+
+  }).bind("drag", function(e){
+    console.log("drag");
+
   });
 });
 
@@ -30,6 +52,7 @@ function uploadFiles(files) {
         XHR.upload.addEventListener('progress',function(e){
           progre = parseInt(e.loaded/e.total*10000)/100;
           console.log(progre+"%");
+          $("#progress-bar").width(progre + "%");
         }, false);
       }
       return XHR;
