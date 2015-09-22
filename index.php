d2e23cc18ba576ed235f71fed61499efbec28ba9
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2015-09-22 11:49:37 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>アップロードサンプル</title>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="/upload_form.js"></script>
+  </head>
+  <body>
+    <h1>アップロードサンプル</h1>
+    <p>ブラウザにファイルをドロップするとファイルをアップロードできます。</p>
+    
+  </body>
+</html>
